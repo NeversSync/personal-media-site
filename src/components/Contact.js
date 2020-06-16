5258529ead19feb9a96899fba1f350f305371132
Nevers <livelyevers@gmail.com>
2020-06-15 17:20:45 -0700

    Update vars

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,8 @@ import { navigate } from 'gatsby-link'
 import Fade from 'react-reveal';
 import Recaptcha from "react-google-recaptcha";
 
+const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY;
 const recaptchaRef = React.createRef();
-const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 
 function encode(data) {
   return Object.keys(data)
